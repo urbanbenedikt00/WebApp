780571a54f4c503c0389fc2e3770be5520df0478
Kaddour Alnaasan <30522984+kalnaasan@users.noreply.github.com>
2023-03-26 12:27:53 +0000

    init Project

---
+++ bean-beispiel/src/main/java/edu/fra/uas/beanbeispiel/BeanBeispielApplication.java
@@ -0,0 +1,13 @@
+package edu.fra.uas.beanbeispiel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanBeispielApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeanBeispielApplication.class, args);
+	}
+
+}
