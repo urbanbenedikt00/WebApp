ac1a4a9425a6406c29f3601b82f55cda42a25958
Kaddour Alnaasan <30522984+kalnaasan@users.noreply.github.com>
2023-04-07 14:53:51 +0000

    create BeanBeispiel

---
+++ bean-beispiel/src/main/java/edu/fra/uas/beanbeispiel/BeanBeispielApplication.java
@@ -0,0 +1,28 @@
+package edu.fra.uas.beanbeispiel;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class BeanBeispielApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeanBeispielApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(){
+		CommandLineRunner action = new CommandLineRunner() {
+			
+			@Override
+			public void run(String... args) throws Exception {
+				System.out.println("Hallo World");
+			}
+			
+		};
+		return action;
+	}
+
+}
