daf01fa05d7332e6f597a788f00e73d8ee4dbfa3
Kaddour Alnaasan <30522984+kalnaasan@users.noreply.github.com>
2023-04-07 15:43:46 +0000

    create BeanBeispielExtendedLogging

---
+++ bean-beispiel-extended-logging/src/main/java/edu/fra/uas/beanbeispielextendedlogging/service/MessageService.java
@@ -0,0 +1,17 @@
+package edu.fra.uas.beanbeispielextendedlogging.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageService {
+    
+    private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
