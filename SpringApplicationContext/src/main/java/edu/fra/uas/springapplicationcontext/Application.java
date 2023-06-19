2cb0ef489a8d0289436b6714750ede712469174d
Kaddour Alnaasan <kaddour.alnaasan@bellavista-film.com>
2023-06-19 14:06:14 +0200

    Revert "delete dummy projects"
    This reverts commit 8cb3501770a14602afd00be0af0f9f9b909499a2.

---
+++ SpringApplicationContext/src/main/java/edu/fra/uas/springapplicationcontext/Application.java
@@ -0,0 +1,13 @@
+package edu.fra.uas.springapplicationcontext;
+
+public class Application {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
