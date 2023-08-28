33297a9b8b4bb309c7ffa62def5d60b55d04ba48
Kaddour Alnaasan <30522984+kalnaasan@users.noreply.github.com>
2023-08-28 11:10:05 +0000

    add HTTP-Mapping annotations

--- rest-crud/src/main/java/edu/fra/uas/curd/controller/UserController.java
+++ rest-crud/src/main/java/edu/fra/uas/curd/controller/UserController.java
@@ -7,7 +7,10 @@ import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,15 +26,16 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    // http://127.0.0.1/
+    // http://127.0.0.1:8000/
     @RequestMapping
 	public String get() {
         log.debug("get() is called");
 		return "index.html";
 	}
 
-    // http://127.0.0.1/users/list
+    // http://127.0.0.1:8000/users/list
     @RequestMapping(value = {"/users/list"}, method = RequestMethod.GET)
+    // @GetMapping(value = {"/users/list"})
     public String list(Model model) {
         log.debug("list() is called");
         Iterable<User> userIter = userService.getAllUsers();
@@ -43,8 +47,9 @@ public class UserController {
         return "list.html";
     }
 
-    // http://127.0.0.1/users/find?id=1
+    // http://127.0.0.1:8000/users/find?id=1
     @RequestMapping(value = {"/users/find"}, method = RequestMethod.GET)
+    // @GetMapping(value = {"/users/find"})
     public String getById(@RequestParam("id") Long userId, Model model) {
         log.debug("find() is called");
         User user = userService.getUserById(userId);
@@ -52,8 +57,9 @@ public class UserController {
         return "find.html";
     }
 
-    // http://127.0.0.1/users/add?firstName=Celine&lastName=Clever&email=celine.clever%40example.com&password=123456
+    // http://127.0.0.1:8000/users/add?firstName=Celine&lastName=Clever&email=celine.clever%40example.com&password=123456
     @RequestMapping(value = {"/users/add"}, method = RequestMethod.GET)
+    // @GetMapping(value = {"/users/add"})
     public String add(@RequestParam("firstName") String firstName, 
                       @RequestParam("lastName") String lastName, 
                       @RequestParam("email") String email, 
@@ -71,15 +77,18 @@ public class UserController {
         return "add.html";
     }
 
-    // http://127.0.0.1/users/update
+    // http://127.0.0.1:8000/users/update
     @RequestMapping(value = {"/users/update"}, method = RequestMethod.GET)
+    // @GetMapping(value = {"/users/update"})
     public String update() {
         log.debug("update() is called");
         return "update.html";
     }
 
-    // http://127.0.0.1/users/updated?id=2&firstName=Alice&lastName=Abraham&email=alice%40example.com&password=123A456
+    // http://127.0.0.1:8000/users/updated?id=2&firstName=Alice&lastName=Abraham&email=alice%40example.com&password=123A456
     @RequestMapping(value = {"/users/updated"}, method = { RequestMethod.GET, RequestMethod.POST })
+    // @GetMapping(value = {"/users/update"})
+    // @PutMapping(value = {"/users/update"})
     public String update(@RequestParam("id") Long userId, 
                          @RequestParam("firstName") String firstName, 
                          @RequestParam("lastName") String lastName, 
@@ -97,8 +106,9 @@ public class UserController {
         return "updated.html";
     }
 
-    // http://127.0.0.1/users/delete/3
+    // http://127.0.0.1:8000/users/delete/3
     @RequestMapping(value = {"/users/delete/{id}"}, method = RequestMethod.GET)
+    // @DeleteMapping(value = {"/users/delete/{id}"})
     public String delete(@PathVariable("id") Long id, Model model) {
         log.debug("delete() is called");
         User user = userService.getUserById(id);
