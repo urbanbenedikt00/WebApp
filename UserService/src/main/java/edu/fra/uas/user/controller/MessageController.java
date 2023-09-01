26387f86ea315b6c4af4e96eee601e24b6cd3680
Kaddour Alnaasan <kaddour.alnaasan@bellavista-film.com>
2023-09-01 15:52:50 +0200

    delete unused MessageController

--- UserService/src/main/java/edu/fra/uas/user/controller/MessageController.java
+++
@@ -1,9 +0,0 @@
-package edu.fra.uas.user.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping(value = { "/messages" })
-public class MessageController {
-}
