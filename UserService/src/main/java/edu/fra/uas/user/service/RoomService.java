00fb3c7139667a09c453d5a808f28e4e60c81a8a
Kaddour Alnaasan <kaddour.alnaasan@bellavista-film.com>
2023-09-01 15:54:25 +0200

    Fix Response of Methods

--- UserService/src/main/java/edu/fra/uas/user/service/RoomService.java
+++ UserService/src/main/java/edu/fra/uas/user/service/RoomService.java
@@ -3,9 +3,10 @@ package edu.fra.uas.user.service;
 import edu.fra.uas.user.model.Message;
 import edu.fra.uas.user.model.Room;
 import edu.fra.uas.user.model.User;
+import org.springframework.http.ResponseEntity;
 
 public interface RoomService {
-    Room joinRoom(Long roomId, User user);
-    Boolean leaveRoom(Long roomId, Long userId);
-    Message sendMessage(Long roomId, Message message);
+    ResponseEntity<?> joinRoom(Long roomId, User user);
+    ResponseEntity<?> leaveRoom(Long roomId, Long userId);
+    ResponseEntity<?> sendMessage(Long roomId, Message message);
 }
